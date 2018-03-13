da820db2b8cb9f0b526d1efc27639bf5caa182cc
Lauri Kananen <lauri.kananen@gmail.com>
2018-03-13 16:41:45 +0200

    Modified cpp

--- src/BitHackLogger/bithacklogger.cpp
+++ src/BitHackLogger/bithacklogger.cpp
@@ -1,8 +1,27 @@
 #include "bithacklogger.h"
+#include "../loglevel.h"
 
+using namespace bithack;
 
-static bithack::ILogger* create()
+static ILogger* create()
 {
 	bithack::ILogger* logger = new bithack::ILogger;
+	logger->setLevel(0);
 	return logger;
+}
+
+
+ILogger::~ILogger()
+{
+
+}
+
+void ILogger::write(LogLevel level, const char* file, uint32_t line, const std::string& text)
+{
+
+}
+
+void ILogger::setLevel(LogLevel level)
+{
+
 }
