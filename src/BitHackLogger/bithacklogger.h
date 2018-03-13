80d28543bf78229f3bfc1b7bb997c5e905e42586
Lauri Kananen <lauri.kananen@gmail.com>
2018-03-13 16:10:54 +0200

    Toteutettavat funktot

--- src/BitHackLogger/bithacklogger.h
+++ src/BitHackLogger/bithacklogger.h
@@ -8,6 +8,8 @@ class BitHackLogger
 {
 public:
 	static bithack::ILogger* create();
+    virtual void write(LogLevel level, const char* file, uint32_t line, const std::string& text);
+    virtual void setLevel(LogLevel level);
 };
 
 #endif /* SRC_BITHACKLOGGER_BITHACKLOGGER_H_ */
