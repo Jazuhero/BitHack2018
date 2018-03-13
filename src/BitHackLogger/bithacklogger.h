4ee4379a33e3104d74fc20c53940787262d497ac
Lauri Kananen <lauri.kananen@gmail.com>
2018-03-13 16:17:37 +0200

    Removed include

--- src/BitHackLogger/bithacklogger.h
+++ src/BitHackLogger/bithacklogger.h
@@ -2,14 +2,11 @@
 #define SRC_BITHACKLOGGER_BITHACKLOGGER_H_
 
 #include "../ilogger.h"
-#include "../loglevel.h"
 
 class BitHackLogger 
 {
 public:
 	static bithack::ILogger* create();
-    virtual void write(LogLevel level, const char* file, uint32_t line, const std::string& text);
-    virtual void setLevel(LogLevel level);
 };
 
 #endif /* SRC_BITHACKLOGGER_BITHACKLOGGER_H_ */
