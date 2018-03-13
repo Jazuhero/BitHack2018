1fc5dde7db41dacd43b3ec091d39c6e67c6b4b79
Arttu Ruusiala <arttu.ruusiala@student.tut.fi>
2018-03-13 17:02:22 +0200

    setLevel definition added

--- src/BitHackLogger/bithacklogger.cpp
+++ src/BitHackLogger/bithacklogger.cpp
@@ -8,6 +8,9 @@ class MyLogger : public ILogger
 public:
 	virtual void write(LogLevel level, const char* file, uint32_t line, const std::string& text);
 	virtual void setLevel(LogLevel level);
+private:
+	LogLevel level_;
+
 };
 
 void MyLogger::write(LogLevel level, const char* file, uint32_t line, const std::string& text)
@@ -17,12 +20,12 @@ void MyLogger::write(LogLevel level, const char* file, uint32_t line, const std:
 
 void MyLogger::setLevel(LogLevel level)
 {
-
+	level_ = level;
 }
 
 ILogger* BitHackLogger::create()
 {
 	ILogger* logger = new MyLogger();
-	logger->setLevel(LogLevel::LOG_LEVEL_DEFAULT);
+	logger->setLevel(LOG_LEVEL_DEFAULT);
 	return logger;
 }
